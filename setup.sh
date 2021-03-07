c4f9ddf18cea8db47cb054af5e8f510710b4901f
Ruan Bekker <rbekker87@gmail.com>
2021-03-07 22:07:21 +0200

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+
+xcode-select --install
+ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
+brew update
+brew install cask
+brew install --cask iterm2
+
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+brew install --cask --appdir="/Applications" virtualbox
+brew install --cask --appdir="/Applications" vagrant
+brew install --cask --appdir="/Applications" visual-studio-code
+brew install --appdir="/Applications" jq
+brew install --appdir="/Applications" wget
+brew install --build-from-source fzf
+brew install --appdir="/Applications" kubernetes-cli
+
+/usr/local/opt/fzf/install
+source ~/.zshrc
+
+brew install --appdir="/Applications" go
+brew install --cask multipass
+
+brew tap hashicorp/tap
+brew install hashicorp/tap/terraform
+brew upgrade hashicorp/tap/terraform
